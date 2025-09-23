627a6eacbaaf8e0352b16ae53b67783af45ae009
NaviGamage <navinduprabashwara@gmail.com.com>
2025-09-23 16:24:30 +0530

    Added ItemManagementService interface with CRUD operations

--- src/main/java/controller/itemcontroller/ItemManagementService.java
+++ src/main/java/controller/itemcontroller/ItemManagementService.java
@@ -1,4 +1,14 @@
 package controller.itemcontroller;
 
+import javafx.collections.ObservableList;
+import model.Item;
+
 public interface ItemManagementService {
+    ObservableList<Item> getAllItems();
+
+    void addItems (String itemCode, String desc, String packSize, double unitPrize, int qty);
+
+    void deleteItem(String itemCode);
+
+    void updateItem(String itemCode, String desc, String packSize, double unitPrize, int qty);
 }
