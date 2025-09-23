67e6b02925b626e9cae4c0e7dd3d3fafd3b63857
NaviGamage <navinduprabashwara@gmail.com.com>
2025-09-23 16:52:11 +0530

    Added ItemManagementFormController with CRUD functionality and table view support and update item_Management.fxml

--- src/main/java/controller/itemcontroller/ItemManagementFormController.java
+++ src/main/java/controller/itemcontroller/ItemManagementFormController.java
@@ -1,13 +1,25 @@
 package controller.itemcontroller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Item;
 
-public class ItemManagementFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemManagementFormController implements Initializable {
+
+    ObservableList<Item> items = FXCollections.observableArrayList();
+
+    ItemManagementService itemManagementService = new ItemManagementController();
 
     @FXML
     private Button btnAdd;
@@ -37,7 +49,7 @@ public class ItemManagementFormController {
     private TableColumn<?, ?> colUnitPrice;
 
     @FXML
-    private TableView<?> tblItems;
+    private TableView<Item> tblItems;
 
     @FXML
     private TextField txtDesc;
@@ -57,21 +69,97 @@ public class ItemManagementFormController {
     @FXML
     void btnAddOnAction(ActionEvent event) {
 
+        String ItemCode = txtItemCode.getText();
+        String Desc = txtDesc.getText();
+        String PackSize = txtPackSize.getText();
+        double UnitPrize = Double.parseDouble(txtUnitPrice.getText());
+        int Qty = Integer.parseInt(txtQty.getText());
+
+        itemManagementService.addItems(ItemCode , Desc , PackSize , UnitPrize , Qty);
+
+        loadItemDetails();
+
+        clearData();
+
     }
 
     @FXML
     void btnClearOnAction(ActionEvent event) {
+
+        clearData();
 
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
 
+        itemManagementService.deleteItem(txtItemCode.getText());
+
+        loadItemDetails();
+
+        clearData();
+
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
 
+        String ItemCode = txtItemCode.getText();
+        String Desc = txtDesc.getText();
+        String PackSize = txtPackSize.getText();
+        double UnitPrize = Double.parseDouble(txtUnitPrice.getText());
+        int Qty = Integer.parseInt(txtQty.getText());
+
+        itemManagementService.updateItem(ItemCode , Desc , PackSize , UnitPrize , Qty);
+
+        loadItemDetails();
+
+        clearData();
+
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("ItemCode"));
+        colDesc.setCellValueFactory(new PropertyValueFactory<>("Desc"));
+        colPackSize.setCellValueFactory(new PropertyValueFactory<>("PackSize"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("UnitPrice"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("Qty"));
+
+        loadItemDetails();
+
+        tblItems.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            if(newValue != null){
+                System.out.println(newValue);
+
+                setSelectedValue(newValue);
+
+            }
+        });
+    }
+
+    private void loadItemDetails(){
+
+        items.clear();
+
+        items = itemManagementService.getAllItems();
+
+        tblItems.setItems(items);
+    }
+
+    private void setSelectedValue(Item selectedValues){
+        txtItemCode.setText(selectedValues.getItemCode());
+        txtDesc.setText(selectedValues.getDesc());
+        txtPackSize.setText(selectedValues.getPackSize());
+        txtUnitPrice.setText(String.valueOf(selectedValues.getUnitPrice()));
+        txtQty.setText(String.valueOf(selectedValues.getQty()));
+    }
+
+    private void clearData(){
+        txtItemCode.setText(null);
+        txtDesc.setText(null);
+        txtPackSize.setText(null);
+        txtUnitPrice.setText(null);
+        txtQty.setText(null);
+    }
 }
