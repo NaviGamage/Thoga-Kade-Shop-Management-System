d7aa7677115bf5da64a611002371cb39166e413c
NaviGamage <navinduprabashwara@gmail.com.com>
2025-09-23 12:13:19 +0530

    create the iteam management form userinterface and create itemmanagemnetformcontroller class

---
+++ src/main/java/controller/itemcontroller/ItemManagementFormController.java
@@ -0,0 +1,77 @@
+package controller.itemcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ItemManagementFormController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colDesc;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colPackSize;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private TableView<?> tblItems;
+
+    @FXML
+    private TextField txtDesc;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtPackSize;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
