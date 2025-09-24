a8ad1f76b02d062a40834339dbc52b561ad1bb98
NaviGamage <navinduprabashwara@gmail.com.com>
2025-09-24 15:27:13 +0530

    Added Order model class

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+    private String ID;
+    private String Date;
+    private String CustID;
+
+}
